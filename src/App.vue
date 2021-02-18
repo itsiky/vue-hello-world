e924ab4301f05e463eda53449b24e4eab64fd730
Itsik Yehuda <itsiky@gmail.com>
2021-02-18 18:40:01 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Itsik Yehuda" id="23423454545"/>
   </div>
 </template>
 
