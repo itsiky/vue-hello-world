532cfeff2caaa84952a10c6356eaf8e96485d7c0
Itsik Yehuda <itsiky@gmail.com>
2021-02-18 18:45:09 +0200

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h3>id: {{ id }}</h3>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
